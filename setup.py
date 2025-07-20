ff3b62e6f7968f87fc2d16a9499acd0661a78113
Dipanjan Jayswal <iamdipanjanjayswal@gmail.com>
2025-07-21 01:46:49 +0530

    added the required requirements and tested the API's

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='document_portal',
     version='0.1.0',
     author='Dipanjan Jayswal',
     packages=find_packages(),
